e2f028801df4ec326e4296cf6bb6ec2a62cc6c52
Dister_07 <136187045+Dister0909@users.noreply.github.com>
2024-10-30 23:49:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-let tg = window.Telegram.WebApp;
-let sendData = "";
-
-let MainButton = WebApp.MainButton;
+var MainButton = WebApp.MainButton;
+var BackButton = WebApp.BackButton;
 
 MainButton.show();
+BackButton.show();
 
-MainButton.onClick(function () {
-       let area = document.getElementById('area').value;
-        let thickness = document.getElementById('thickness').value;
-        let price_stone = document.getElementById('price_stone').value;
-        let price_primer = document.getElementById('price_primer').value;
-        
-        let volume = area * thickness; 
-        let mass_stone = volume * 778.29; // +
-        let mass_primer = volume * 62.6; // +
-                
-        let cost_stone = mass_stone * price_stone; // +
-        let cost_primer = mass_primer * price_primer; // +
-        let total_cost = cost_stone + cost_primer; // +
-        
-        sendData = mass_stone + "$" + mass_primer + "$" + cost_stone + "$" + cost_primer + "$" + total_cost;
+MainButton.onClick(function() {
+  WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
+});
+WebApp.onEvent('mainButtonClicked', function() {
+  /* also */
+});
 
-        tg.sendData(sendData); 
+BackButton.onClick(function() {
+  WebApp.showAlert("Нет пути назад!");
+  
+  BackButton.hide();
+});
+WebApp.onEvent('backButtonClicked', function() {
+  /* also */
 });
